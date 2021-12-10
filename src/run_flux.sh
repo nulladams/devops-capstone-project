07904ab252800b7eaf01e3625264fbfd6932b801
Leonardo Adams <leonardoadams@gmail.com>
2021-12-10 17:40:45 -0300

    clean file

---
+++ src/run_flux.sh
@@ -0,0 +1,8 @@
+$GHUSER = "nulladams"
+fluxctl install \
+--git-user=${GHUSER} \
+--git-email=${GHUSER}@users.noreply.github.com \
+--git-url=git@github.com:${GHUSER}/devops-capstone-project \
+--git-path=src/k8s \
+--git-branch=main \
+--namespace=flux | kubectl apply -f -
