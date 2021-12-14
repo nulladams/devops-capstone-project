6cacf40b816bd87e96f4b016d5aa335643672ef7
Leonardo Adams <leonardoadams@gmail.com>
2021-12-14 17:55:35 -0300

    change page to blue 2

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ def scale(payload):
 
 @app.route("/")
 def home():
-    html = f"<h3>Sklearn Prediction Home -blue 1</h3>"
+    html = f"<h3>Sklearn Prediction Home -blue 2 </h3>"
     return html.format(format)
 
 @app.route("/predict", methods=['POST'])
